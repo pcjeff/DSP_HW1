b2da2421810c8597d75a675588d6c7f8bac3082b
jeff Lin <pcjeff@cml10.csie.ntu.edu.tw>
2015-11-06 13:45:06 +0800

    output accuracy to acc.txt

--- EvalAcc.py
+++ EvalAcc.py
@@ -2,19 +2,19 @@ import sys
 predict = []
 answers = []
 
-with open(sys.argv[1], 'r') as result_f, open(sys.argv[2], 'r') as answer_f:
+with open(sys.argv[1], 'r') as result_f, open(sys.argv[2], 'r') as answer_f, open("acc.txt", "w") as acc_f:
     for line in result_f:
         predict.append(line.strip().split()[0])
     for line in answer_f:
         answers.append(line.strip().split()[0])
 
-correct = 0.
-wrong = 0.
+    correct = 0.
+    wrong = 0.
 
-for ind in range(len(predict)):
-    if(predict[ind] == answers[ind]):
-        correct += 1
-    else:
-        wrong += 1
-
-print correct / ind
+    for ind in range(len(predict)):
+        if(predict[ind] == answers[ind]):
+            correct += 1
+        else:
+            wrong += 1
+            
+    print >> acc_f, correct / ind
